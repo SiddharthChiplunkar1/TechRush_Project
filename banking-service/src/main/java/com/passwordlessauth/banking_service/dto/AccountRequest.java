6442ea4ac747f7d881079ccdc895abbe53fd1fca
Siddharth Chiplunkar <chiplunkar.siddharth@gmail.com>
2026-08-05 20:08:14 +0530

    Delete banking-service directory

--- banking-service/src/main/java/com/passwordlessauth/banking_service/dto/AccountRequest.java
+++
@@ -1,4 +0,0 @@
-package com.passwordlessauth.banking_service.dto;
-
-public class AccountRequest {
-}
--- banking-service/src/main/java/com/passwordlessauth/banking_service/enums/TransactionStatus.java
+++
@@ -1,2 +0,0 @@
-package com.passwordlessauth.banking_service.enums;
-public enum TransactionStatus { COMPLETED, BLOCKED_STEP_UP_REQUIRED, FAILED }
