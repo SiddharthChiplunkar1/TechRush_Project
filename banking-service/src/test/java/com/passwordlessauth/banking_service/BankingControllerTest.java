004ea0a58d0e4f20de70b0c79dc4470c833b191d
Siddharth <admin@campusearn.com>
2026-08-05 07:46:53 +0530

    Minor Changes Fixed

--- banking-service/src/test/java/com/passwordlessauth/banking_service/BankingControllerTest.java
+++ banking-service/src/test/java/com/passwordlessauth/banking_service/BankingControllerTest.java
@@ -1,15 +1,24 @@
 package com.passwordlessauth.banking_service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
+import com.passwordlessauth.banking_service.service.BankingService;
 
 class BankingControllerTest {
 
     @Test
     void healthEndpointReturnsServiceStatus() {
-        BankingController controller = new BankingController();
+
+        BankingService bankingService = mock(BankingService.class);
+
+        BankingController controller =
+                new BankingController(bankingService);
+
         Map<String, String> response = controller.health();
 
         assertEquals("banking-service", response.get("service"));
